3e7fd9023959bb04de72ea8b28e101b882a65177
Albert Baffour Awuah <baffour006@gmail.com>
2021-05-18 18:09:27 +0200

    asp.worx

---
+++ ContosoUniversity/ContosoUniversity/Migrations/20210511090325_first.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Student",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Classroom",
+                columns: table => new
+                {
+                    ClassroomID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Number = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classroom", x => x.ClassroomID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    TeacherID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(nullable: true),
+                    FirstMidName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.TeacherID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classroom");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Student");
+        }
+    }
+}
---
+++ MusicStore/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MusicStore.Data;
+using MusicStore.Models;
+using MusicStore.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreContext _context;
+
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger, StoreContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListGenres()
+        {
+            var genres = _context.Genres.OrderBy(g=>g.Name);
+            return View(genres.ToList());
+        }
+        public IActionResult ListAlbums(int id)
+        {
+            var listalbumsVM = new ListAlbumsViewModel();
+            listalbumsVM.genre = _context.Genres.FirstOrDefault(g => g.GenreID == id);
+            listalbumsVM.Albums = (_context.Albums
+                .Where(a=>a.GenreID==id)
+                .OrderBy(g => g.Title)).ToList();
+            return View(listalbumsVM);
+        }
+        public IActionResult Details(int id)
+        {
+            var album = _context.Albums
+                .Include(a => a.Artist)
+                .Include(a => a.Genre)
+                .SingleOrDefault(a=> a.AlbumID==id);
+            return View(album);
+        }
+     
+    }
+}
